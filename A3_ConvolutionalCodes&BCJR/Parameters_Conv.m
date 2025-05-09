759b42023b689e0411ae621fa0bc6eb906639000
Gabriele Liga <g.liga@tue.nl>
2025-05-09 15:43:29 +0200

    Modified Parameters_Conv in A3.

--- A3_ConvolutionalCodes&BCJR/Parameters_Conv.m
+++ A3_ConvolutionalCodes&BCJR/Parameters_Conv.m
@@ -4,8 +4,8 @@
 %%% Convolutional code parameters %%%%%%%%%%%
 Nbits=1e5;     % Number of info bits per simulated block 
 K=4;           % Convolutional encoder constraint length 
-poly=[13,15];   % [5,7];   % Set of polynomials defining convolutional encoder
-trellis=poly2trellis(K,poly);                        % Trellis structure
+poly=[17,15];           %[13,15];   % [5,7];   % Set of polynomials defining convolutional encoder
+trellis=poly2trellis(K,poly,17);                        % Trellis structure
 r=size(poly,1)/size(poly,2);    % Convolutional code rate 
 tb=1e5;                         % Decoding window
 DecType='unquant';
@@ -18,4 +18,4 @@ EbNo=2:0.2:7;    % Eb/No [dB]
 %% Error estimation parameters 
 FERcheck=1;                  % Simulation controlled on frame error numbers 
 BERcheck=0;                  % Simulation controlled on bit error numbers 
-TargetErr=5e2;               % Target error numbers 
+TargetErr=1e2;               % Target error numbers 
